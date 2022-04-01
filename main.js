6fe38a57221a7ad4a109c3ce8051258f271aef49
samarthmudgal <90464416+samarthmudgal@users.noreply.github.com>
2022-04-01 12:50:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+ block_image_width = 30;
+ block_image_height= 30;
+
+ player_x = 10;
+ player_y = 10;
+
+ var player_object="";
+
+ function player_update(){
+
+ fabric.Image.fromURL("player.png",function(Img) {
+     player_object = Img;
+
+
+     player_object.scaleToWidth(150);
+     player_object.scaleToHeight(140);
+     player_object.set({
+         top:player_y,
+         top:player_x
+     });
+     canvas.add(player_object);
+ });
+}
+
+function new_image(get_image)
+{
+    fabric.Image.fromURL(get_image, function(Img) {
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_height.scaleToHeight(block_image_height);
+        block_image_object.set({
+         top:player_y,
+         left:player_x
+
+        });
+     canvas.add(block_image_object);
+
+    });
+}
